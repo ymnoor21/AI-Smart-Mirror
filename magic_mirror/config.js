2516f6e10966809820ff6599804d2f4c2c1eb569
Yamin Noor <ymnoor21@gmail.com>
2017-01-05 11:18:11 -0800

    Updated with more config

--- magic_mirror/config.js
+++ magic_mirror/config.js
@@ -10,17 +10,128 @@ var config = {
 
 	language: 'en',
 	timeFormat: 24,
-	units: 'metric',
+	units: 'imperial',
 
 	modules: [
-    {
-        module: 'aiclient',
-        position: 'middle_center' // This can be any of the regions.
-    },
-    {
-    	module: 'aiclientdebugger',
-    	position: 'bottom_right'
-    }
+        {
+            module: 'aiclient',
+            position: 'middle_center' // This can be any of the regions.
+        },
+        {
+        	module: 'aiclientdebugger',
+        	position: 'bottom_right'
+        },
+        {
+            module: 'clock',
+            position: 'top_left',
+            config: {
+                'displayType': 'both',
+                'analogSize': '200px',
+                'analogFace': 'face-005',
+                'secondsColor': '#ffffff'
+            }
+        },
+        {
+            module: 'calendar',
+            header: 'VIEDU Programmers',
+            position: 'top_left',
+            config: {
+                calendars: [
+                    {
+                        'symbol': 'calendar',
+                        'url': 'https://calendar.google.com/calendar/ical/viedu.org_6j9lp4jo72uaife6s3iu3o53k8%40group.calendar.google.com/public/basic.ics'
+                    }
+                ]
+            }
+        },
+        {
+            module: 'calendar',
+            header: 'My Calendar',
+            position: 'top_left',
+            config: {
+                calendars: [
+                    {
+                        'symbol': 'calendar',
+                        'url': 'https://calendar.google.com/calendar/ical/ymnoor21%40gmail.com/private-c4126a04bb38426c23ccb2c6a875fb80/basic.ics'
+                    }
+                ]
+            }
+        },
+        {
+            module: 'calendar',
+            header: 'US Holidays',
+            position: 'top_left',
+            config: {
+                calendars: [
+                    {
+                        symbol: 'calendar',
+                        url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+                    }
+                ]
+            }
+        },
+        {
+            module: 'currentweather',
+            position: 'top_right',
+            config: {
+                location: 'Folsom, CA',
+                locationID: '',  //ID from http://www.openweathermap.org
+                appid: 'e15ca5d4d80c3a45e4de2028bd640a13'
+            }
+        },
+        {
+            module: 'weatherforecast',
+            position: 'top_right',
+            header: 'Weather Forecast',
+            config: {
+                        location: 'Folsom, CA',
+                locationID: '',  //ID from http://www.openweathermap.org
+                        appid: 'e15ca5d4d80c3a45e4de2028bd640a13'
+            }
+        },
+        {
+            module: 'newsfeed',
+            position: 'bottom_bar',
+            config: {
+                feeds: [
+                    {
+                        title: "Prothom Alo",
+                        url: "http://www.prothom-alo.com/feed/",
+                        encoding: 'utf-8'
+                    }
+                ],
+                showSourceTitle: true,
+                showPublishDate: true
+            }
+        },
+        {
+            module: 'newsfeed',
+            position: 'bottom_bar',
+            config: {
+                feeds: [
+                    {
+                        title: "The Daily Star",
+                        url: "http://www.thedailystar.net/latest/rss/rss.xml"
+                    }
+                ],
+                showSourceTitle: true,
+                showPublishDate: true
+            }
+        },
+        {
+            module: 'newsfeed',
+            position: 'bottom_bar',
+            config: {
+                feeds: [
+                    {
+                        title: "New York Times",
+                        url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+                    }
+                ],
+                showSourceTitle: true,
+                showPublishDate: true
+            }
+        }
 	]
 
 };
