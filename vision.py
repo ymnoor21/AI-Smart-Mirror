c993f6c51aad8074464d0a3ce26579ee7e35a267
Yamin Noor <ymnoor21@gmail.com>
2017-01-22 13:28:46 -0800

    Create face dataset

--- vision.py
+++ vision.py
@@ -15,7 +15,6 @@ class Vision(object):
         self.camera = camera
 
     def detect_face(self):
-        # face_cascade = cv2.CascadeClassifier('models/classifiers/haarcascades/haarcascade_frontalface_default.xml')
         face_cascade = cv2.CascadeClassifier(self.facial_recognition_model)
         video_capture = cv2.VideoCapture(self.camera)
 
@@ -41,6 +40,44 @@ class Vision(object):
             cv2.imshow('Video', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything is done, release the capture
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+    def create_face_dataset(self):
+        face_cascade = cv2.CascadeClassifier(self.facial_recognition_model)
+        video_capture = cv2.VideoCapture(self.camera)
+        id = raw_input("Enter user id: ")
+        counter = 0
+
+        while True:
+            # Capture frame-by-frame
+            ret, frame = video_capture.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                counter = counter + 1
+                cv2.imwrite('models/userimages/user.'+str(id)+"."+str(counter)+".jpg", gray[y:y+h, x:x+w])
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.waitKey(100)
+
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
+            cv2.waitKey(1)
+
+            if counter >= 20:
                 break
 
         # When everything is done, release the capture
@@ -83,4 +120,4 @@ class Vision(object):
 
 if __name__ == "__main__":
     v = Vision()
-    v.detect_face()
+    v.create_face_dataset()
