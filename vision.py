b4888b2bdb602300163a032662361d4b4ef190d2
Yamin Noor <ymnoor21@gmail.com>
2017-01-22 12:00:39 -0800

    Add face detection

--- vision.py
+++ vision.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 vision_enabled = False
 try:
@@ -12,6 +13,39 @@ class Vision(object):
     def __init__(self, facial_recognition_model="models/facial_recognition_model.xml", camera=0):
         self.facial_recognition_model = facial_recognition_model
         self.camera = camera
+
+    def face_detect(self):
+        # face_cascade = cv2.CascadeClassifier('models/classifiers/haarcascades/haarcascade_frontalface_default.xml')
+        face_cascade = cv2.CascadeClassifier(self.facial_recognition_model)
+        video_capture = cv2.VideoCapture(self.camera)
+
+        while True:
+            # Capture frame-by-frame
+            ret, frame = video_capture.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything is done, release the capture
+        video_capture.release()
+        cv2.destroyAllWindows()
 
     def recognize_face(self):
         """
@@ -48,34 +82,5 @@ class Vision(object):
 
 
 if __name__ == "__main__":
-    faceCascade = cv2.CascadeClassifier("models/facial_recognition_model.xml")
-
-    video_capture = cv2.VideoCapture(0)
-
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything is done, release the capture
-    video_capture.release()
-    cv2.destroyAllWindows()
+    v = Vision()
+    v.face_detect()
