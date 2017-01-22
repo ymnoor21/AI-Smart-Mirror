bf1ec28fd64e044225889c301a518c82ea4f2162
Yamin Noor <ymnoor21@gmail.com>
2017-01-22 13:34:20 -0800

    Remove print

--- vision.py
+++ vision.py
@@ -107,7 +107,6 @@ class Vision(object):
             faceNp = np.array(faceImg, 'uint8')
             ID = int(os.path.split(imagePath)[-1].split('.')[1])
             faces.append(faceNp)
-            print(ID)
             IDs.append(ID)
             cv2.imshow('training', faceNp)
             cv2.waitKey(10)
